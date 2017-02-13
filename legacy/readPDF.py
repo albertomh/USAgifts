e088dfaea82434316c326bed76a328e947385b67
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2017-02-13 15:56:43 +0000

    Iterate through .pdfs and dump to .txt files.

--- legacy/readPDF.py
+++ legacy/readPDF.py
@@ -11,3 +11,16 @@ def readPDF():
         if file.endswith(".pdf"):
             l_pdfs.append(file)
     print(l_pdfs)
+
+    
+# Read each file referred to in 'l_pdfs' and writes ocntents to 'datadump$YEAR.txt'.
+
+    for pdf in l_pdfs:
+
+        with open(dirpath + pdf, 'rb') as pdfFileObj:
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+            pdfyear = ''.join(x for x in pdf if x.isdigit())
+            with open(''.join(['datadump', pdfyear, '.txt']), 'a') as txtdump:
+                for page in range(pdfReader.getNumPages()):
+                    txtdump.write(pdfReader.getPage(page).extractText())
