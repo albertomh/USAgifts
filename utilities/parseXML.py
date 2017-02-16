1c47ba50abfdfd3154ba305582f734a2bdc9f03c
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-02-16 09:07:04 +0000

    Print info and time taken.

--- utilities/parseXML.py
+++ utilities/parseXML.py
@@ -12,20 +12,32 @@ def extract(year):
 
     """
 
+# Initiate variables.
     start_time = time.perf_counter()
     tree = ET.parse(dirpath + 'xml\\' + str(year) + '.xml')
     l_data = []
 
+# Iterate through XML and extract content.
     for child in tree.iter():
         if child.tag == str('ROW'):
             l_temp = [item.strip() for item in child.itertext()]
 
+# Discard incomplete records, write content to l_data.
             l_temp = list(filter(None, l_temp))
             if len(l_temp) == 4:
                 l_data.append(l_temp)
 
-    with open('outputTXT\\' + 'out' + str(year) + '.txt', 'wb') as outfile:
+# Write data out to a .txt file.
+    with open(dirpath + 'txt\\' + str(year) + '.txt', 'wb') as outfile:
         outfile.write(b'[')
         for row in l_data:
             outfile.write('{}, '.format(row).encode('utf8'))
         outfile.write(b']')
+
+# Print out stats
+    time_taken = time.perf_counter() - start_time
+    print(str(year) + ': '
+          + str(len(l_data))
+          + ' gifts recorded. | That took:'
+          + '{0:.1f}'.format(time_taken)
+          + 'seconds.\n')
