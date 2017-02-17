0384a97e0caf70c35211f6f4bd97fc01b918b7d0
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-02-17 20:34:35 +0000

    clean() normalises nation names.

---
+++ utilities/read.py
@@ -0,0 +1,25 @@
+import ast
+
+dirpath = r'$PATH'
+
+
+def clean(year):
+    """
+    Normalise nation names.
+
+    """
+
+# Get data in from year.txt and dictionary of replacement strings.
+    with open(dirpath + 'txt\\' + str(year) + '.txt', encoding='utf8') as infile:
+        indata = infile.read()
+    giftdata = indata
+    with open(dirpath + 'utilities\\' + 'd_replacements.txt', encoding='utf8') as d_infile:
+        replacements = ast.literal_eval(d_infile.read())
+
+# Replace strings found in d_replacements.txt.
+    for k, v in replacements.items():
+        giftdata = giftdata.replace(k, v)
+
+# Write data back to year.txt.
+    with open(dirpath + 'txt\\' + str(year) + '.txt', 'wb') as outfile:
+        outfile.write(giftdata.encode('utf8'))
