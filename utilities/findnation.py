393ef3a18b19f0b13d8249df570440be48578a61
Alberto Morón Hernández <alberto.moronh@gmail.com>
2017-02-20 10:35:00 +0000

    Returns nations that did not gift that year.

--- utilities/findnation.py
+++ utilities/findnation.py
@@ -2,9 +2,12 @@ import ast
 from read import read
 
 
-def findnation(year):
+def findnation(year, *args):
     """
     Returns a list of every nation that gifts in a given year.
+    findnation(year).count('nation') returns number of gifts from a single nation.
+
+    Run findnation(year, "missing") to return list of records missing gifter nation data.
 
     """
 
@@ -19,3 +22,11 @@ def findnation(year):
     for row in range(len(giftdata)):
         nationmatch = [x for x in nations if x in giftdata[row][2]]
         contained.extend(nationmatch)
+
+# Code to evaluate *args and return all records missing gifter nation.
+        if args:
+            if not nationmatch:
+                print(giftdata[row][2], '\n\n')
+
+    contained = list(set(contained))  # Remove set to return every instance.
+    return contained
